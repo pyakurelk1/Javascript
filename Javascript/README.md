47438b0bb922f97e4786c488024e05098868e988
pyakurelk1 <122557490+pyakurelk1@users.noreply.github.com>
2023-03-18 22:12:05 -0400

    Add files via upload

---
+++ Javascript/README.md
@@ -0,0 +1,2 @@
+Here is my github link for the code:
+https://github.com/pyakurelk1/Javascript
---
+++ Javascript/index.js
@@ -0,0 +1,8 @@
+function Hello_W() {
+    console.log("Hello, world!");
+  }
+  
+  module.exports = {
+    Hello_W: Hello_W
+  };
+  
